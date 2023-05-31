9274bf7b07436a657f414bbb0dcf2adfb59a877a
Austin Croucher <austin.croucher@gmail.com>
2023-05-31 12:38:07 -0400

    create room working

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 
 // Create a new data structure to hold room IDs
 const rooms = {};
+const usersInRooms = {};
 
 const messages = {};
 
@@ -30,11 +31,47 @@ app.use(express.json());
 
 // Create a new route to handle room creation
 app.post('/create-room', (req, res) => {
-    const roomId = Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit number
+    // Generate a unique room ID
+    const roomId = Math.floor(Math.random() * 10000);
 
-    // TODO: Check if the room ID already exists, and if it does, generate a new one
+    // Get the user ID from the request body or session
+    const userId = req.body.userId;
 
-    rooms[roomId] = {}; // Initialize the room
+    // Add the room to our rooms object with the host's user ID
+    rooms[roomId] = { hostId: userId, users: [] };
 
-    res.json({ roomId: roomId.toString() }); // Convert the room ID to a string and send it back to the client
+    // Return the room ID in the response
+    res.json({ roomId, isHost: true });
 });
+
+app.post('/join-room/:roomId', (req, res) => {
+    const { roomId } = req.params;
+    const { userId } = req.body;
+
+    if (rooms[roomId]) {
+        // Add the user to the room
+        rooms[roomId].users.push(userId);
+
+        // Return a response indicating whether the user is the host
+        const isHost = rooms[roomId].hostId === userId;
+        res.json({ success: true, isHost });
+    } else {
+        // Return a response indicating that the room does not exist
+        res.json({ success: false });
+    }
+});
+
+app.get('/room-exists/:roomId', (req, res) => {
+    const { roomId } = req.params;
+
+    if (rooms[roomId]) {
+        // Return a response indicating that the room exists
+        res.sendStatus(200);
+    } else {
+        // Return a response indicating that the room does not exist
+        res.sendStatus(404);
+    }
+});
+
+
+app.use(cors(corsOptions));
