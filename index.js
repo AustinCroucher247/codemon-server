152428cdb711bb50d8241ec3852632971d698a03
Austin Croucher <austin.croucher@gmail.com>
2023-05-31 14:30:34 -0400

    getting closer with rooms showing all users

--- index.js
+++ index.js
@@ -1,77 +1,87 @@
-// import necessary dependencies
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
-require('dotenv').config();
-const PORT = 8080;
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
-// Create a new data structure to hold room IDs
 const rooms = {};
 const usersInRooms = {};
-
 const messages = {};
 
-const server = app.listen(PORT, () => {
-    console.log(`Server listening on port 8080`);
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
 });
 
-const io = socketIo(server, { transports: ['websocket', 'polling'] });
-
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: 'http://localhost:3000',
     credentials: true,
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
-// Create a new route to handle room creation
 app.post('/create-room', (req, res) => {
-    // Generate a unique room ID
     const roomId = Math.floor(Math.random() * 10000);
-
-    // Get the user ID from the request body or session
     const userId = req.body.userId;
-
-    // Add the room to our rooms object with the host's user ID
     rooms[roomId] = { hostId: userId, users: [] };
-
-    // Return the room ID in the response
     res.json({ roomId, isHost: true });
 });
 
 app.post('/join-room/:roomId', (req, res) => {
     const { roomId } = req.params;
-    const { userId } = req.body;
+    const { userId, username } = req.body;
 
     if (rooms[roomId]) {
-        // Add the user to the room
-        rooms[roomId].users.push(userId);
-
-        // Return a response indicating whether the user is the host
+        rooms[roomId].users.push({ userId, username });
         const isHost = rooms[roomId].hostId === userId;
-        res.json({ success: true, isHost });
+        const usernamesInRoom = rooms[roomId].users.map(user => user.username);
+        console.log('Usernames in room:', usernamesInRoom);
+        res.json({ success: true, isHost, usernamesInRoom });
     } else {
-        // Return a response indicating that the room does not exist
         res.json({ success: false });
     }
 });
 
 app.get('/room-exists/:roomId', (req, res) => {
     const { roomId } = req.params;
-
     if (rooms[roomId]) {
-        // Return a response indicating that the room exists
         res.sendStatus(200);
     } else {
-        // Return a response indicating that the room does not exist
         res.sendStatus(404);
     }
 });
 
+io.on('connection', (socket) => {
+    console.log('New client connected');
 
-app.use(cors(corsOptions));
+    socket.on('joinRoom', ({ roomId, userId, username }) => {
+        socket.join(roomId);
+
+        if (!rooms[roomId]) {
+            rooms[roomId] = {
+                hostId: userId,
+                users: [],
+            };
+        }
+
+        const user = { userId, username };
+        rooms[roomId].users.push(user);
+
+        io.to(roomId).emit('roomUsers', { users: rooms[roomId].users });
+
+        console.log(`User with ID ${userId} joined room with ID ${roomId}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(8080, () => {
+    console.log(`Server listening on port 8080`);
+});
